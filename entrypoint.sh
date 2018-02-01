63868cf88bd6e1312f1c4d2f79df7ecf0be14c69
楼兰 <zhaihuailou@icloud.com>
2018-02-01 21:23:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ touch /mosquitto/passwd
 mosquitto_passwd -U /mosquitto/passwd
 mosquitto_passwd -b /mosquitto/passwd $MQTT_USERNAME $MQTT_PASSWORD
 
+echo $MQTT_USERNAME 
+echo $MQTT_PASSWORD
+
 exec "$@"
