9c5959e419abc3bb112986ab452e1564e6f43471
楼兰 <zhaihuailou@icloud.com>
2018-02-01 10:57:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PASSWDFILE=/etc/mosquitto/passwd
-USERNAME=mqtt
+MQTT_PASSWDFILE=/mosquitto/passwd
 
 if [ -f $PASSWDFILE ]; then
-    mosquitto_passwd -U $PASSWDFILE
+    mosquitto_passwd -U $MQTT_PASSWDFILE
+    mosquitto_passwd -b $MQTT_PASSWDFILE $MQTT_USERNAME $MQTT_PASSWORD
 fi
 
 exec "$@"
